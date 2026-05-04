3c3aed5490e7dc95b0e2ddf399cd518aa22d2823
TristanR-lol <tristan.raneroyay@proton.me>
2026-05-04 10:27:20 +0100

    Adaptive tweaks for popup.

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
+let params = new URLSearchParams(document.location.search);
+let redirect = params.get("redirect") || "https://ilan.bloxxer.dev/";
 document.getElementById("Continue").addEventListener("click", function () {
-    let params = new URLSearchParams(document.location.search);
-    let redirect = params.get("redirect") || "https://ilan.bloxxer.dev/";
     location.href = redirect + "?accepted=true";
 });
+document.getElementById("HBD").innerHTML=`${redirect}: Here Be Dragons!`;
